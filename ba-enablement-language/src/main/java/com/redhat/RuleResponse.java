d95cbf8d5a53a202f2dc7ba34a5fbfe64890db8e
Branden Pleines <bpleines@redhat.com>
2015-09-03 15:02:21 -0700

    app with working bpmn2 process (xor gates, process variables)

--- ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
+++ ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
@@ -7,8 +7,10 @@ public class RuleResponse {
 	@KieQuery(binding = "$business", queryName = "Get Business")
 	private Business business;
 	
+	@KieQuery(binding = "$responseCode", queryName = "Get Response Code")
 	private String responseCode; // e.g. success, failure, filtered, validation error...
 	
+	@KieQuery(binding = "$responseCode", queryName = "Get Reason Messages")
 	private Collection<Reason> reasons; // messages about why the response Code came bacl
 
 	public Business getBusiness() {
@@ -17,6 +19,14 @@ public class RuleResponse {
 
 	public void setBusiness(Business business) {
 		this.business = business;
+	}
+
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	public Collection<Reason> getReasons() {
+		return reasons;
 	}
 
 }
